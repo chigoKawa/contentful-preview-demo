b4085784df02982f98660c2a2aa57728afdc1ca4
chigoKawa <kawa.chigozirim@gmail.com>
2021-07-27 20:22:02 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,57 @@
-import Head from 'next/head'
+import Head from "next/head";
+import _ from "lodash";
+import Layout from "../components/layout";
+import {  getAllPostsForHome, getAllLocales } from "../util/api";
+import HeroPost from "../components/heroPost";
+import MorePosts from "../components/morePosts";
 
-export default function Home() {
+import dayjs from "dayjs";
+let advancedFormat = require("dayjs/plugin/advancedFormat");
+dayjs.extend(advancedFormat);
+
+export default function Home(props) {
+  console.log("PAGES", props);
+  const allPosts = _.get(props, "allPosts");
+  const preview = _.get(props, "preview");
+  const heroPost = _.get(allPosts, "[0]");
+  const morePosts = allPosts.slice(1);
+
+  const date = _.get(heroPost, "fields.date");
+  const title = _.get(heroPost, "fields.title");
+  const excerpt = _.get(heroPost, "fields.excerpt");
+  const slug = _.get(heroPost, "fields.slug");
+  let postDate = dayjs(date).format(" dddd Do MMMM, YYYY");
+  const fields = _.get(allPosts, "fields");
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <Layout preview={preview}>
+        <hr />
+        <div className="px-4 md:px-20 lg:px-40">
+          <HeroPost
+            slug={slug}
+            excerpt={excerpt}
+            title={title}
+            date={postDate}
+            data={heroPost}
+          />
+          <br />
+          <hr />
+          <br />
+          {morePosts.length > 0 && <MorePosts posts={morePosts} />}
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </Layout>
+      <div className=""></div>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const allPosts = (await getAllPostsForHome(preview)) ?? [];
+
+  const allLocales = (await getAllLocales()) ?? [];
+
+  return {
+    props: { preview, allPosts, allLocales },
+  };
 }
