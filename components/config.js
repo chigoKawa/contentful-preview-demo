bb6f56f0da75aab94ad9cd717eb506c889dc6616
chigoKawa <kawa.chigozirim@gmail.com>
2021-07-27 20:41:01 +0200

    rough readme sketch

--- components/config.js
+++
@@ -1,9 +0,0 @@
-// All environment variables will be sourced
-// Do NOT commit this file to source control
-module.exports = {
-space_id: '3cgdpp6k1aen',
-delivery_token: 'sctNKTzOuVi8ZUMv5a2d6CGao_5BF053gD4NFxOo8NI',
-preview_token: '5RasaA3XqdtikFeyNRrv8HAH59dPddsxjaj1fP2stZ4',
-previewSecret: 'testing',
-environment: 'master'
-}
