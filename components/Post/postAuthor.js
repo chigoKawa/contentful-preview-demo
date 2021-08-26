042e83aee312aed1d385579fd380095d9ef70455
chigoKawa <kawa.chigozirim@gmail.com>
2021-08-26 10:40:26 +0200

    added key to Link

--- components/Post/postAuthor.js
+++ components/Post/postAuthor.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import _ from "lodash";
 
-
 const PostAuthor = (props) => {
   const author = _.get(props, "author");
   const authorImage = _.get(props, "authorImage");
@@ -10,23 +9,23 @@ const PostAuthor = (props) => {
   return (
     <div className="flex flex-row items-center mb-4 mt-4">
       <div className="w-14 h-14 relative md:mr-2 ">
+        {" "}
         {authorImage ? (
           <Image
             src={`http:${authorImage}`}
             layout="fill"
             className="rounded-full"
             alt={author}
+            noOptimization
           />
         ) : (
           ""
-        )}
-      </div>
+        )}{" "}
+      </div>{" "}
       <div className="md:mr-2 whitespace-nowrap flex flex-col">
-        <span> {author}</span>
-        <span className="text-center font-bold text-xs">{date}</span>
-       
-        
-        </div>
+        <span> {author} </span>{" "}
+        <span className="text-center font-bold text-xs"> {date} </span>
+      </div>{" "}
     </div>
   );
 };
